a470c49824e2e417c82e048290d33f440a47db49
Abhishek Mishra <abhimishraofficial@gmail.com>
2025-11-04 10:56:42 +0530

    Initial commit - my version of Algorithm Finder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from 'react';
+import HomePage from "./components/HomePage"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomePage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
